0a38bc4ea64e296b019d3e05b2071ed3bf0e27e3
Omda Farm <dev@omda.farm>
2025-11-08 10:22:04 +0200

    Dark mode cards: switch to chalk background and borders (no navy)

--- farm-manager/src/components/ui/Card.tsx
+++ farm-manager/src/components/ui/Card.tsx
@@ -1,11 +1,11 @@
 import { cn } from '../../lib/utils'
 
 export function Card({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn('rounded-lg border bg-white dark:bg-slate-950', className)} {...props} />
+  return <div className={cn('rounded-lg border bg-white dark:bg-[#0f1111] dark:border-[#1a1c1c]', className)} {...props} />
 }
 
 export function CardHeader({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn('p-4 border-b', className)} {...props} />
+  return <div className={cn('p-4 border-b dark:border-[#1a1c1c]', className)} {...props} />
 }
 
 export function CardTitle({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) {
